80ca3a87dbf407499064c345686cd9e7b5073bef
Nick Charlton <hello@nickcharlton.net>
2013-11-09 12:52:41 +0000

    Implements a simple Sinatra based OmniAuth example.

--- example/app.rb
+++ example/app.rb
@@ -2,7 +2,31 @@ class App < Sinatra::Base
   use Rack::Session::Cookie
   use OmniAuth::Strategies::PAM
 
+  helpers do
+    def ensure_auth
+      unless session[:auth]
+        redirect '/auth/pam'
+      end
+    end
+  end
+
   get '/' do
-    'Hello World'
+    ensure_auth
+  end
+
+  # OmniAuth support
+  post '/auth/:name/callback' do
+    auth = request.env['omniauth.auth']
+    puts auth
+
+    <<-HTML
+    <h1>Authenticated via #{auth[:provider]}.</h1>
+
+    <ul>
+      <li>name: #{auth.info[:name]}</li>
+      <li>nickname: #{auth.info[:nickname]}</li>
+      <li>uid: #{auth[:uid]}</li>
+    </ul>
+    HTML
   end
 end
