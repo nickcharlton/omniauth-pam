e901304a19d1cb2ac2320560118b761c4f2d83e3
Nick Charlton <nick@nickcharlton.net>
2018-05-19 22:23:27 +0100

    Add tests. (#13)
    * Aims to cover the interaction between PAM + OmniAuth.
    * Mocks `rpam`, `etc` to allow it to run without directly
      authenticating.
    * Reworks the class loading such that the test paths can be simplified,
      and it behave similar to `omniauth-identity`.

--- lib/omniauth-pam.rb
+++ lib/omniauth-pam.rb
@@ -1,7 +1,2 @@
-# depends upon rpam and etc
-require "rpam"
-require "etc"
-
-# then the rest of the strategy
 require "omniauth-pam/version"
-require "omniauth/strategies/pam"
+require "omniauth/pam"
